ed675193168dd0eea3ea6150fea5b12e65ef01af
dlavric <daniela@hashicorp.com>
2021-04-14 11:19:53 +0200

    updated the repo

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# vagrant-vault-download
+# Installs an Unix Vagrant machine with any version of Vault is specified
+
+### Repository has been created using [this repo](https://github.com/kikitux/vault-dev-orcl).
+
+
+## Prerequisites
+- [X] [VirtualBox](https://www.virtualbox.org/wiki/Downloads)
+- [X] [Vagrant](https://www.vagrantup.com/downloads)
+
+## Instructions
+
+- Copy the repository and go to the repo path:
+
+```shell
+$ git clone git@github.com:dlavric/vagrant-vault-download.git
+$ cd vagrant-vault-download
+```
+
+- Start Vagrant and provision it:
+
+```shell
+$ vagrant up
+$ vagrant provision
+```
---
+++ scripts/install_vault.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# check for tools we need
+which curl wget unzip jq &>/dev/null || {
+  yum install -y curl wget unzip jq
+}
+
+# VAULT variable is set in the Vagrantfile
+# if not set, we go for the latest version
+
+[ "${VAULT}" ] || {
+  VAULT=$(curl -sL https://releases.hashicorp.com/vault/index.json | jq -r '.versions[].version' | sort -V | egrep -v 'ent|beta|rc|alpha' | tail -n1)
+}
+
+
+cp /vagrant/sw/hosts /etc/hosts
+echo 'vault.test' > /etc/hostname
+hostname vault.test
+
+export PATH=/usr/local/bin:${PATH}
+
+cd /usr/local/bin
+[ -f vault_${VAULT}_linux_amd64.zip ] || {
+  wget https://releases.hashicorp.com/vault/${VAULT}/vault_${VAULT}_linux_amd64.zip
+}
+
+unzip -o vault_${VAULT}_linux_amd64.zip
+
+[ -f /etc/vault.d/server.hcl ] || {
+  curl -sL https://raw.githubusercontent.com/kikitux/curl-bash/master/vault-dev-inmem/vault.sh | bash
+}
+
+cd /vagrant
+[ -d sw ] || mkdir sw
+cd sw
+touch vault.service
+yes | cp /etc/systemd/system/vault.service /vagrant/sw/vault.service
+
+systemctl daemon-reload
+systemctl restart vault
+
+cp /root/.vault-token /home/vagrant/.vault-token
+chown -R vagrant: /home/vagrant/.vault-token
+export VAULT_ADDR=http://localhost:8200
