45abd6aab992b2f8c3a44dc6b564e91e75c6e167
Alvaro Miranda Aguilera <kikitux@gmail.com>
2021-04-15 13:22:26 +0200

    update

--- scripts/install_vault.sh
+++ scripts/install_vault.sh
@@ -2,7 +2,7 @@
 
 # check for tools we need
 which curl wget unzip jq &>/dev/null || {
-  yum install -y curl wget unzip jq
+  apt-get install -y curl wget unzip jq
 }
 
 # VAULT variable is set in the Vagrantfile
@@ -12,33 +12,24 @@ which curl wget unzip jq &>/dev/null || {
   VAULT=$(curl -sL https://releases.hashicorp.com/vault/index.json | jq -r '.versions[].version' | sort -V | egrep -v 'ent|beta|rc|alpha' | tail -n1)
 }
 
-
-cp /vagrant/sw/hosts /etc/hosts
-echo 'vault.test' > /etc/hostname
-hostname vault.test
-
-export PATH=/usr/local/bin:${PATH}
-
-cd /usr/local/bin
+cd /vagrant
 [ -f vault_${VAULT}_linux_amd64.zip ] || {
   wget https://releases.hashicorp.com/vault/${VAULT}/vault_${VAULT}_linux_amd64.zip
 }
 
-unzip -o vault_${VAULT}_linux_amd64.zip
+cd /usr/local/bin
+unzip -o /vagrant/vault_${VAULT}_linux_amd64.zip
 
 [ -f /etc/vault.d/server.hcl ] || {
   curl -sL https://raw.githubusercontent.com/kikitux/curl-bash/master/vault-dev-inmem/vault.sh | bash
 }
-
-cd /vagrant
-[ -d sw ] || mkdir sw
-cd sw
-touch vault.service
-yes | cp /etc/systemd/system/vault.service /vagrant/sw/vault.service
 
 systemctl daemon-reload
 systemctl restart vault
 
 cp /root/.vault-token /home/vagrant/.vault-token
 chown -R vagrant: /home/vagrant/.vault-token
-export VAULT_ADDR=http://localhost:8200
+
+export VAULT_ADDR=http://0.0.0.0:8200
+vault version
+vault status
